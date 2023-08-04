c6326a1dd93e98d06d6629e99e561d10c8c0e6d2
Simon Mourier <simon_mourier@hotmail.com>
2023-08-04 18:57:10 +0200

    Added IPropertyStore.

--- DirectN/DirectN/Manual/IPropertyStore.cs
+++ DirectN/DirectN/Manual/IPropertyStore.cs
@@ -3,9 +3,22 @@ using System.Runtime.InteropServices;
 
 namespace DirectN
 {
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("886d8eeb-8cf2-4446-8d02-cdba1dbdcf99")]
-    public interface IPropertyStore
+    [ComImport, Guid("886d8eeb-8cf2-4446-8d02-cdba1dbdcf99"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public partial interface IPropertyStore
     {
-        // not yet defined
+        [PreserveSig]
+        HRESULT GetCount(/* [out] __RPC__out */ out uint cProps);
+
+        [PreserveSig]
+        HRESULT GetAt(/* [in] */ uint iProp, /* [out] __RPC__out */ out PROPERTYKEY pkey);
+
+        [PreserveSig]
+        HRESULT GetValue(/* [in] __RPC__in */ ref PROPERTYKEY key, /* [out] __RPC__out */ [In, Out] PropVariant pv);
+
+        [PreserveSig]
+        HRESULT SetValue(/* [in] __RPC__in */ ref PROPERTYKEY key, /* [in] __RPC__in */ PropVariant propvar);
+
+        [PreserveSig]
+        HRESULT Commit();
     }
 }
