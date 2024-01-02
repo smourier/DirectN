1eece80edf05330cb700d54cf45ed395b8cc7fff
Simon Mourier <simon_mourier@hotmail.com>
2024-01-02 10:55:41 +0100

    Updated copyright dates

--- DirectN/DirectN.WinUI3.MinimalD3D11/Properties/AssemblyInfo.cs
+++ DirectN/DirectN.WinUI3.MinimalD3D11/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("Simon Mourier")]
 [assembly: AssemblyProduct("DirectN")]
 [assembly: AssemblyDescription("Interop Code for .NET : DXGI, WIC, DirectX 9 to 12, Direct2D, Direct Write, Direct Composition, Media Foundation, WASAPI, CodecAPI, GDI, Spatial Audio, DVD, Windows Media Player, UWP DXInterop, etc.")]
-[assembly: AssemblyCopyright("Copyright © 2017-2023 Simon Mourier. All rights reserved. Original code by d7samurai.")]
+[assembly: AssemblyCopyright("Copyright © 2017-2024 Simon Mourier. All rights reserved. Original code by d7samurai.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("99873e46-b92d-47ce-b5be-d453cad3895b")]
