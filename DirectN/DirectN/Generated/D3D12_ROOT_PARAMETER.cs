365df1708003ab02b4260ce01fe3a9b898dbb377
Simon Mourier <simon_mourier@hotmail.com>
2024-01-29 12:31:52 +0100

    Fix D3D12_ROOT_PARAMETER definition (pack/pad issue), only for 64 bit

--- DirectN/DirectN/Generated/D3D12_ROOT_PARAMETER.cs
+++ DirectN/DirectN/Generated/D3D12_ROOT_PARAMETER.cs
@@ -7,7 +7,9 @@ namespace DirectN
     public partial struct D3D12_ROOT_PARAMETER
     {
         public D3D12_ROOT_PARAMETER_TYPE ParameterType;
+        public uint __pad0;
         public D3D12_ROOT_PARAMETER__union_0 __union_1;
         public D3D12_SHADER_VISIBILITY ShaderVisibility;
+        public uint __pad1;
     }
 }
