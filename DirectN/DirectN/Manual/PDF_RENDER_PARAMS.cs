ac2cc472447526f52e999981b950e57d4c558867
Simon Mourier <smourier@users.noreply.github.com>
2024-02-16 11:38:02 +0100

    Added PDF rendering structure

---
+++ DirectN/DirectN/Manual/PDF_RENDER_PARAMS.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DirectN
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PDF_RENDER_PARAMS
+    {
+        public D2D_RECT_F SourceRect;
+        public int DestinationWidth;
+        public int DestinationHeight;
+        public _D3DCOLORVALUE BackgroundColor;
+        public bool IgnoreHighContrast;
+    }
+}
