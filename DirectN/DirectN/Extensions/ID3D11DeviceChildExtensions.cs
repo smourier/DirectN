315181e734a9401579a0f6fb211487ce57c6ff6b
Simon Mourier <smourier@users.noreply.github.com>
2024-09-04 08:11:04 +0200

    Added some utilities

--- DirectN/DirectN/Extensions/ID3D11DeviceChildExtensions.cs
+++ DirectN/DirectN/Extensions/ID3D11DeviceChildExtensions.cs
@@ -15,5 +15,16 @@ namespace DirectN
                 _ = child.SetPrivateData(Constants.WKPDID_D3DDebugObjectNameW, (uint)(name.Length * 2), mem.Pointer).ThrowOnError();
             }
         }
+
+        public static IComObject<ID3D11Device> GetDevice(this IComObject<ID3D11DeviceChild> child) => GetDevice<ID3D11Device>(child?.Object);
+        public static IComObject<T> GetDevice<T>(this IComObject<ID3D11DeviceChild> child) where T : ID3D11Device => GetDevice<T>(child?.Object);
+        public static IComObject<T> GetDevice<T>(this ID3D11DeviceChild child) where T : ID3D11Device
+        {
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+
+            child.GetDevice(out var device);
+            return new ComObject<T>((T)device);
+        }
     }
 }
